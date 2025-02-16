9409118482f9b03541dc7a2aec76672e5e8dfbac
guilhermefrazao <guilhermefrazao@discente.ufg.br>
2025-02-16 14:10:36 -0300

    Update multi_gpu

--- rats/multi_gpu.py
+++ rats/multi_gpu.py
@@ -1,16 +1,45 @@
 import ray
 import time
-import torch
-from ray.util.multiprocessing import Pool
-#from yolov10_model import YOLOv10
+import torchvision
+import os
+import torch 
 
-ray.init()
 
-@ray.remote
-def f(i):
-    time.sleep(1)
-    return i
+from ultralytics import ASSETS, YOLO
 
-futures = [f.remote(i) for i in range(4)]
-print(ray.get(futures))
-pool = Pool(ray_address="177.205.85.62:")
+
+@ray.remote(num_gpus=1)
+def Yolov8_train():
+    model = YOLO("yolov8n.pt", task="detect")
+    
+    results = model.train(data="C:/Users/guilh/Documents/GitHub_clone/Yolov11/datasets/data.yaml", device=0, epochs=100, imgsz=640, batch=8, workers=8, project="runs/detect/train15_Yolov8_augmentation", name="train15_Yolov8_augmentation", exist_ok=True)
+    
+    return results
+
+def Yolov8_val():
+    model = YOLO("runs/detect/train15_Yolov8_augmentation/weights/best_tuned.pt", task="detect")
+    
+    results = model.val(data="C:/Users/guilh/Documents/GitHub_clone/Yolov11/datasets/data.yaml", device=0, epochs=100, imgsz=640, batch=8, workers=8, project="runs/detect/train15_Yolov8_augmentation", name="train15_Yolov8_augmentation", exist_ok=True)
+    
+    return results
+
+
+def Yolov8_test():
+    model = YOLO("runs/detect/train15_Yolov8_augmentation/weights/best_tuned.pt", task="detect")
+    
+    results = model.test(data="C:/Users/guilh/Documents/GitHub_clone/Yolov11/datasets/data.yaml", device=0, epochs=100, imgsz=640, batch=8, workers=8, project="runs/detect/train15_Yolov8_augmentation", name="train15_Yolov8_augmentation", exist_ok=True)
+    
+    return results
+
+def Run_remote():
+    ray.init()
+
+    ray.get([Yolov8_train.remote() for _ in range(os.cpu_count())])
+
+if __name__ == "__main__":
+    print(torch.__version__)
+    print(torchvision.__version__)
+    print(torch.cuda.is_available())
+    print(torch.version.cuda)
+
+    Run_remote()
